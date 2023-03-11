7e819e92099c43a14f880f8a2b72e3645ab821f0
Hamza Malik <muhammadhamzamlk@gmail.com>
2023-03-11 19:49:46 +0500

    Hamza Malik

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import Footer from './Footer';
 import HeroSection from './Hero';
 import Cards from './Cards'
+import Prac from './Prac'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
