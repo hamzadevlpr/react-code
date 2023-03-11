a0f7ed3d641f7ff3cb248965add893da7739a8ac
Hamza Malik <muhammadhamzamlk@gmail.com>
2023-03-11 20:52:21 +0500

    11/03/2023

--- src/Prac.jsx
+++ src/Prac.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 
 export default function Prac() {
+  const name = [
+    "Hamza",
+    "Malik",
+    "Khan",
+    "Jan",
+    "Ahsan"
+  ]
+  const weekdays = [
+    "Monday",
+    "Tuesday",
+    "Wedness",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+  ]
+  let date = new Date();
   return (
     <div>
-      
+
+      <h1>{weekdays[date.getUTCDay() - 1]}, {name[Math.trunc(Math.random() * 5) ]} </h1>
+
     </div>
   )
 }
+
--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Footer from './Footer';
-import HeroSection from './Hero';
-import Cards from './Cards'
-import Prac from './Prac'
+import Prac from './Prac';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Footer/>
-    <HeroSection/>
-    <Cards/>
+    <Prac/>
   </React.StrictMode>
 );
